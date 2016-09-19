8736e6b025723fb4c25423b1047e29415b88f203
anandnalya <anand.nalya@gmail.com>
2016-09-19 14:53:27 +0530

    Updated for Livy v0.2.0

--- build_parcel.sh
+++ build_parcel.sh
@@ -13,35 +13,32 @@ PARCEL_DIR=LIVY-$1
 PARCEL=$PARCEL_DIR-$2.parcel
 
 # Build Livy
-[ ! -d ./hue ] && git clone --depth 1 https://github.com/cloudera/hue.git
+[ ! -d ./livy ] && git clone https://github.com/cloudera/livy.git
 
-cd ./hue/apps/spark/java
+cd ./livy
 
-mvn -DskipTests clean package
+git checkout v0.2.0
+
+mvn -DskipTests -Dspark.version=1.6.0-cdh5.8.0 -Dhadoop-version=2.6.0-cdh5.8.0 clean package
 
 # Prepare parcel
-cd ../../../..
+cd ../
 
 [ ! -d ./$PARCEL_DIR ] && rm -rf ./$PARCEL_DIR
 
-mkdir -p ./$PARCEL_DIR/lib
+mkdir -p ./$PARCEL_DIR/jars
+mkdir -p ./$PARCEL_DIR/repl-jars
+mkdir -p ./$PARCEL_DIR/rsc-jars
 
-cp -r ./hue/apps/spark/java/bin ./$PARCEL_DIR/
-cp -r ./hue/apps/spark/java/conf ./$PARCEL_DIR/
-cp hue/apps/spark/java/livy-assembly/target/scala-2.10/*.jar ./$PARCEL_DIR/lib/
-
-# Download logback jar
-wget -O ./$PARCEL_DIR/lib/logback-classic.jar http://central.maven.org/maven2/ch/qos/logback/logback-classic/1.1.3/logback-classic-1.1.3.jar
-
-# Set classpath
-sed -i -e 's|livy-assembly/target/scala-2.10|lib|g' ./$PARCEL_DIR/bin/setup-classpath
+cp -r ./livy/bin ./$PARCEL_DIR/
+cp -r ./livy/conf ./$PARCEL_DIR/
+cp ./livy/server/target/jars/*.jar ./$PARCEL_DIR/jars/
+cp ./livy/repl/target/jars/*.jar ./$PARCEL_DIR/repl-jars/
+cp ./livy/rsc/target/jars/*.jar ./$PARCEL_DIR/rsc-jars/
 
 cp -r parcel-src/meta $PARCEL_DIR/
 
 sed -i -e "s/%VERSION%/$1/" ./$PARCEL_DIR/meta/*
-
-# Add logback to classpath
-echo "CLASSPATH=\"\$ASSEMBLY_DIR/logback-classic.jar:\$CLASSPATH\"" >> ./$PARCEL_DIR/bin/setup-classpath
 
 # Validate and build parcel
 java -jar ~/github/cloudera/cm_ext/validator/target/validator.jar -d ./$PARCEL_DIR
